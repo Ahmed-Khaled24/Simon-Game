b24db86f68f062befa6bae96e3e2e9839a53e07e
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-06-30 11:25:16 +0200

    feat: add computerTrun function
    a function that generate a random click on a button and add it to
    computerInput array

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ var computerInputs = [];
 var buttons = ["red", "green", "yellow", "blue"];
 var level = 1;
 var compareIndex = 0;
+
+function computerTurn(){
+    var computerChoice = buttons[Math.floor(Math.random()*4)];                  // get a random choice from the computer
+    document.querySelector("."+computerChoice).classList.add("btn-pressed");    // Animate the click
+    setTimeout(function(){document.querySelector("." + computerChoice).classList.remove("btn-pressed")}, 200);
+    playSound(computerChoice);
+    computerInputs.push(computerChoice);
+}
